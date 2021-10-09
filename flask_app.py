a37dc515c30ff76b1da645549ecd59d9c22bea2a
greatlygravitate <87620301+greatlygravitate@users.noreply.github.com>
2021-10-09 10:57:14 -0400

    Create flask_app.py

---
+++ flask_app.py
@@ -0,0 +1,12 @@
+from flask import Flask,jsonify
+from geopy.geocoders import Nominatim
+from flask_cors import CORS
+
+app = Flask(__name__)
+cors = CORS(app)
+
+@app.route("/<place>")
+def hello_world(place):
+    geolocator = Nominatim(user_agent="greatlygravitate")
+    location = geolocator.geocode(str(place))
+    return jsonify(str(location.latitude) + "," + str(location.longitude))
